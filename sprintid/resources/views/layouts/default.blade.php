54472c7bfa852ef5c3fae2c3da21f3ac2c8ce59f
jojoarianto <irianto94@outlook.com>
2016-02-21 23:30:19 +0700

    upload laravel source

---
+++ sprintid/resources/views/layouts/default.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>.: SPRINT :.</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Speed Printing - Teknologi baru ngeprint tanpa antri">
+    <meta name="author" content="Sprint ID">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu+Mono' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet/less" type="text" href="style.less">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="less.min.js"></script>
+</head>
+<body class="bg-all-bootstrap">
+
+	@yield('content')
+
+	@include('partials.footer')
+
+	@yield('script')
+
+</body>
+</html>
